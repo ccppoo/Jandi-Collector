7315dfaf1284432d27db3546c74c80e62ab73dd3
ccppoo <shmishmi79@gmail.com>
2021-10-31 00:30:28 +0900

    update gitignore + main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from discordManager import JandiDiscordManager
 import json
 
+
+def main():
+
+    # since gspread is not async
+    # spreadsheetManager and JandiDiscordManager should be run each thread
+
+    return
+
+
 if __name__ == "__main__":
 
     fp = open('./secret.json', mode='r')
